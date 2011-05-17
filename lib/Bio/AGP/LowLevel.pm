3da30bbbfad938cc1e5a10ef122d08bfbc4ef0c0
Sheena Scroggins <sheena.scroggins@gmail.com>
2011-05-17 09:51:27 -0700

    Removed deleted functions from synopsis

--- lib/Bio/AGP/LowLevel.pm
+++ lib/Bio/AGP/LowLevel.pm
@@ -13,16 +13,13 @@ use List::Util qw/first/;
 Bio::AGP::LowLevel - functions for dealing with AGP files
 
 =head1 SYNOPSIS
-
- $bio_large_seq = agp_to_seq($agp_file_name);
+ 
 
  $lines_arrayref = agp_parse('my_agp_file.agp');
 
  agp_write( $lines => 'my_agp_file.agp');
 
- $seq = agp_contig_seq( $mycontig,
-                        fetch_bac_sequences => sub {...}
-                       )
+
 
 =head1 DESCRIPTION
 
